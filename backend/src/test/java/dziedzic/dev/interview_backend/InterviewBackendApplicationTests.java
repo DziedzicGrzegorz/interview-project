59bb9348178cca575c1b9aa7d3392f03d1079c19
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2024-03-22 22:45:22 +0100

    build: Initial Configuration

---
+++ backend/src/test/java/dziedzic/dev/interview_backend/InterviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dziedzic.dev.interview_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
